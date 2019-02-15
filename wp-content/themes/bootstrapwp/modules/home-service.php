4af53557da75130bffac7d4ee6dd194b84172d39
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-02-15 11:05:03 -0700

    Adding the Services Homepage Module

---
+++ wp-content/themes/bootstrapwp/modules/home-service.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * The template for displaying search results pages.
+ *
+ * @package bootstrapwp
+ */
+
+get_header(); ?>
+
+<!-- ==== HEADERWRAP ==== -->
+<div class="pagewrap">
+			<header>
+	  		 		<h1>Our Services</h1>
+	  		</header>	    
+	    </div><!-- /headerwrap -->
+
+
+			
+		<div class="container">
+			<div class="page-content services">
+				<div class="row">
+					<div class="col-lg-12 ">
+
+						<h3 class="text-center">Services We Offer </h3>
+						<p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam mattis ex, ut imperdiet magna imperdiet at. Proin eget pulvinar lorem. Curabitur rhoncus vehicula libero, nec porttitor lectus. Fusce mattis eu leo id tempus. Donec in ultrices nisl. </p>
+					</div>
+					<div class="row">
+						
+					<div class="col-lg-4 callout">
+						<i class="fa fa-rocket"></i>
+						<h2>Bootstrap 3</h2>
+						<p>Diamond Theme is powered by Bootstrap 3. The incredible Mobile First Framework is the best option to run your website. </p>
+					</div><!-- col-lg-4 -->
+						
+					<div class="col-lg-4 callout">
+						<i class="fa fa-eye"></i>
+						<h2>Retina Ready</h2>
+						<p>You can use this theme with your iPhone, iPad or MacBook Pro. This theme is retina ready and that is awesome. </p>
+					</div><!-- col-lg-4 -->	
+					
+					<div class="col-lg-4 callout">
+						<i class="fa fa-heart"></i>
+						<h2>Crafted with Love</h2>
+						<p>We don't make sites, we craft themes with love & passion. That is our most valued secret. We only do thing that we love.   </p>
+					</div><!-- col-lg-4 -->	
+				</div>
+				<div class="row">
+
+					<div class="col-lg-4 callout">
+						<i class="fa fa-globe"></i>
+						<h2>Bootstrap 3</h2>
+						<p>Diamond Theme is powered by Bootstrap 3. The incredible Mobile First Framework is the best option to run your website. </p>
+					</div><!-- col-lg-4 -->
+						
+					<div class="col-lg-4 callout">
+						<i class="fa fa-leaf"></i>
+						<h2>Retina Ready</h2>
+						<p>You can use this theme with your iPhone, iPad or MacBook Pro. This theme is retina ready and that is awesome. </p>
+					</div><!-- col-lg-4 -->	
+					
+					<div class="col-lg-4 callout">
+						<i class="fa fa-group"></i>
+						<h2>Crafted with Love</h2>
+						<p>We don't make sites, we craft themes with love & passion. That is our most valued secret. We only do thing that we love.   </p>
+					</div><!-- col-lg-4 -->	
+
+				</div>
+	
+					</div><!-- col-lg-4 -->	
+				
+			</div>
+		</div>
